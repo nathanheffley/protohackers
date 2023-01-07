29bafcd3fe9fa49c30b96486fa026ac11f0501a7
Nathan Heffley <nathan@nathanheffley.com>
2023-01-06 22:41:55 -0500

    debug

--- 02_means_to_an_end/main.go
+++ 02_means_to_an_end/main.go
@@ -18,12 +18,34 @@ func main() {
 func handle(conn net.Conn) {
 	buf := make([]byte, 1)
 
+	ledger := make(map[int32]int32)
+
 	output := make([]byte, 4)
 
 	for {
 		_, err := conn.Read(buf)
 		if err != nil {
-			break
+			panic(err)
+		}
+
+		messageType := buf[0]
+
+		firstBuf := make([]byte, 4)
+		_, err = conn.Read(firstBuf)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(firstBuf)
+
+		secondBuf := make([]byte, 4)
+		_, err = conn.Read(secondBuf)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(secondBuf)
+
+		if messageType == 'I' {
+			// ledger[]
 		}
 
 		fmt.Println(buf)
