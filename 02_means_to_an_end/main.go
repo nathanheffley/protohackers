495f6906519ee76d297f0d3f0ac470e062ffb789
Nathan Heffley <nathan@nathanheffley.com>
2023-01-06 22:35:56 -0500

    debug

--- 02_means_to_an_end/main.go
+++ 02_means_to_an_end/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"net"
 )
@@ -19,55 +18,15 @@ func main() {
 func handle(conn net.Conn) {
 	buf := make([]byte, 1)
 
-	ledger := make(map[int]int)
-
 	output := make([]byte, 4)
 
 	for {
 		_, err := conn.Read(buf)
 		if err != nil {
-			panic(err)
-		}
-
-		firstBuf := make([]byte, 4)
-		_, err = conn.Read(firstBuf)
-		if err != nil {
-			panic(err)
-		}
-		first := int(binary.BigEndian.Uint32(firstBuf))
-
-		secondBuf := make([]byte, 4)
-		_, err = conn.Read(secondBuf)
-		if err != nil {
-			panic(err)
-		}
-		second := int(binary.BigEndian.Uint32(secondBuf))
-
-		fmt.Println("------")
-		fmt.Println(buf[0], first, second)
-		fmt.Println("------")
-
-		if buf[0] == 'I' {
-			ledger[first] = second
-		}
-
-		if buf[0] == 'Q' {
-			count := 0
-			total := 0
-			for timestamp, price := range ledger {
-				if timestamp >= first && timestamp <= second {
-					count++
-					total += price
-				}
-			}
-
-			if count == 0 {
-				binary.BigEndian.PutUint32(output, 0)
-			} else {
-				binary.BigEndian.PutUint32(output, uint32(total/count))
-			}
 			break
 		}
+
+		fmt.Println(buf)
 	}
 
 	conn.Write(output)
