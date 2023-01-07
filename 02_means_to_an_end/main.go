0258eb2bd9fd634ea4914142499951e6e6e7bd7a
Nathan Heffley <nathan@nathanheffley.com>
2023-01-06 22:23:00 -0500

    debug

--- 02_means_to_an_end/main.go
+++ 02_means_to_an_end/main.go
@@ -29,42 +29,43 @@ func handle(conn net.Conn) {
 			panic(err)
 		}
 
+		firstBuf := make([]byte, 4)
+		_, err = conn.Read(firstBuf)
+		if err != nil {
+			panic(err)
+		}
+		first := int(binary.BigEndian.Uint32(firstBuf))
+
+		secondBuf := make([]byte, 4)
+		_, err = conn.Read(secondBuf)
+		if err != nil {
+			panic(err)
+		}
+		second := int(binary.BigEndian.Uint32(secondBuf))
+
+		fmt.Println("------")
+		fmt.Println(buf[0], first, second)
+		fmt.Println("------")
+
 		if buf[0] == 'I' {
-			timestampBuf := make([]byte, 4)
-			_, err = conn.Read(timestampBuf)
-			if err != nil {
-				panic(err)
-			}
-			timestamp := int(binary.BigEndian.Uint32(timestampBuf))
-
-			priceBuf := make([]byte, 4)
-			_, err = conn.Read(priceBuf)
-			if err != nil {
-				panic(err)
-			}
-			price := int(binary.BigEndian.Uint32(priceBuf))
-
-			fmt.Println(timestamp)
-			fmt.Println(price)
-
-			ledger[timestamp] = price
+			ledger[first] = second
 		}
 
 		if buf[0] == 'Q' {
-			// count := 0
-			// total := 0
-			// for timestamp, price := range ledger {
-			// 	if timestamp >= first && timestamp <= second {
-			// 		count++
-			// 		total += price
-			// 	}
-			// }
+			count := 0
+			total := 0
+			for timestamp, price := range ledger {
+				if timestamp >= first && timestamp <= second {
+					count++
+					total += price
+				}
+			}
 
-			// if count == 0 {
-			binary.BigEndian.PutUint32(output, 0)
-			// } else {
-			// 	binary.BigEndian.PutUint32(output, uint32(total/count))
-			// }
+			if count == 0 {
+				binary.BigEndian.PutUint32(output, 0)
+			} else {
+				binary.BigEndian.PutUint32(output, uint32(total/count))
+			}
 			break
 		}
 	}
