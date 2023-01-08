d543298d12e868e5b3488ff4238dda881e9eb861
Nathan Heffley <nathan@nathanheffley.com>
2023-01-07 21:05:53 -0500

    Messages

--- 03_budget_chat/main.go
+++ 03_budget_chat/main.go
@@ -3,7 +3,31 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
+
+type Client struct {
+	Name string
+	Conn net.Conn
+}
+
+func (c *Client) Write(message string) {
+	c.Conn.Write([]byte(message + "\n"))
+}
+
+func (c *Client) Leave() {
+	c.Conn.Close()
+	newClients := make([]Client, len(clients)-1)
+	for _, client := range clients {
+		if client.Name != c.Name {
+			newClients = append(newClients, client)
+			client.Write(fmt.Sprintf("* %s has left the room", c.Name))
+		}
+	}
+	clients = newClients
+}
+
+var clients = []Client{}
 
 func main() {
 	listener, _ := net.Listen("tcp", "0.0.0.0:8080")
@@ -26,7 +50,37 @@ func handle(conn net.Conn) {
 		return
 	}
 
-	fmt.Println(string(name))
+	client := Client{
+		Name: string(name),
+		Conn: conn,
+	}
 
-	conn.Close()
+	clientNames := make([]string, len(clients))
+	for _, client := range clients {
+		clientNames = append(clientNames, client.Name)
+		client.Write()
+	}
+	roomContainsMessage := fmt.Sprintf("* The room contains: %s\n", strings.Join(clientNames, ", "))
+	client.Write(roomContainsMessage)
+	fmt.Println(roomContainsMessage)
+
+	clients = append(clients, client)
+
+	for {
+		message := make([]byte, 1000)
+		_, err := conn.Read(message)
+		if err != nil {
+			break
+		}
+
+		for _, c := range clients {
+			if c.Name == client.Name {
+				continue
+			}
+
+			c.Write(fmt.Sprintf("[%s] %s", client.Name, string(message)))
+		}
+	}
+
+	client.Leave()
 }
